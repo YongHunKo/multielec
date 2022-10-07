8ae73310d0e02d1c6bb9405315c7e8547175a421
YongHunKo <dydgnsrh5460@naver.com>
2022-10-07 12:28:11 +0900

    multielec basic

---
+++ src/main/java/com/multi/MultielecApplication.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultielecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultielecApplication.class, args);
+	}
+
+}
