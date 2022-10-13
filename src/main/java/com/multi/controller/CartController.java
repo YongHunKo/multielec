ed6b1fdae05963e49850630043b7f8894f8a54e9
YongHunKo <dydgnsrh5460@naver.com>
2022-10-13 17:15:43 +0900

    add payment_221013

--- src/main/java/com/multi/controller/CartController.java
+++ src/main/java/com/multi/controller/CartController.java
@@ -2,27 +2,35 @@ package com.multi.controller;
 
 import java.util.*;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.multi.dto.CartDTO;
+import com.multi.dto.CustDTO;
 import com.multi.service.CartService;
+import com.multi.service.CustService;
 
 @Controller
 public class CartController {
 
 	@Autowired
 	CartService cart_service;
-
+	@Autowired
+	CustService cust_service;
 	
 	@RequestMapping("/cart")
 	public String cart(Model model, String id) {
 		List<CartDTO> list = null;
+		CustDTO cust = null; 
 		try {
 			list = cart_service.cartall(id);
+			cust = cust_service.get(id);
 			model.addAttribute("mycart",list);
+			model.addAttribute("custdetail",cust);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -39,6 +47,32 @@ public class CartController {
 			e.printStackTrace();
 		}
 		return "redirect:cart?id="+custid;
+	}
+	
+	@RequestMapping("/cartpayment")
+	public String cartpayment(Model model, HttpSession session, CustDTO cust) {
+		model.addAttribute("center","/payment");
+		return "redirect:payment?id"+cust.getCustid();
+	}//얘는 이동하는 애
+	
+	@RequestMapping("/payment")
+	public String payment(Model model, String id) {
+		List<CartDTO> list = null;
+		try {
+			list = cart_service.cartall(id);
+			model.addAttribute("mycart",list);
+			model.addAttribute("center","/payment");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/paymentimpl")
+	public String paymentimpl(Model model) {
+		
+		model.addAttribute("center","/paymentok");
+		return "index";
 	}
 }
 
--- src/test/java/com/multi/orderlist/InsertOrderlist.java
+++ src/test/java/com/multi/orderlist/InsertOrderlist.java
@@ -4,19 +4,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.multi.dto.OrderdetailDTO;
 import com.multi.dto.OrderlistDTO;
+import com.multi.service.OrderdetailService;
 import com.multi.service.OrderlistService;
 
 @SpringBootTest
 class InsertOrderlist {
 	@Autowired
-	OrderlistService service;
+	OrderlistService orderlist_service;
+	@Autowired
+	OrderdetailService orderdetail_service;
 
 	@Test
 	void contextLoads() {
+		OrderlistDTO order = new OrderlistDTO(0,"id11", 3, null, 40000);
 		try {
-			service.register(new OrderlistDTO(0,"id11", 3, null, 40000));
-			System.out.println("insert_OK");
+			orderlist_service.register(order);
+			int r = order.getOrderid();
+			System.out.println("insert_OK"+" "+r);
+			OrderdetailDTO orderdetail = new OrderdetailDTO(null, r, null, null, null, null, null);
+			orderdetail_service.register(orderdetail);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
