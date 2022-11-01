fe80751285fc0da5e7b41155e995b97811845a5f
walkwa <citypass0119@naver.com>
2022-11-02 02:03:00 +0900

    add 장바구니 수량변화 test

---
+++ src/test/java/com/multi/carts/ModifyCountCart.java
@@ -0,0 +1,28 @@
+package com.multi.carts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.dto.CartDTO;
+import com.multi.service.CartsServiceImpl;
+
+@SpringBootTest
+class ModifyCountCart {
+
+	@Autowired
+	CartsServiceImpl service;
+	
+	@Test
+	void contextLoads() {
+		CartDTO cart = new CartDTO(3, null, null, 555, null, null, null, null);
+		try {
+			service.modifyCount(cart);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("updated");
+	}
+}
+
